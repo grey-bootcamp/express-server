54d451b6b95e364f055002ec94133b8bf5a8e16d
Abubakar Ahmad <abakargarbalkali@gmail.com>
2022-05-23 13:45:24 +0100

    init commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const express = require('express')
+const app = express()
+app.get("/", (req,res)=>{
+    res.send("hello world")
+})
+app.listen(5000,()=>{
+    console.log("connected")
+})
+
