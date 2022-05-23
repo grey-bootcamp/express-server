b1ecb2b04520408b80977551d59d2e9c386a17f4
boyfrompluto <charlesdavidondom@gmail.com>
2022-05-23 13:46:22 +0100

    charles made a helloworldnsever

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const express = require('express')
+const app= express()
+
+app.get('/', (req, res) => {
+    res.send('hello word')
+})
+app.listen(3000, () => {
+    console.log('connected');
+})
+
+
